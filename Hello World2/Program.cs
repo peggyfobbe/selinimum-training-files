f2b2753a2886afcf5e6c0eceb773d453e532dd1f
Peggy Fobbe <peggyfobbe@hotmail.com>
2016-11-11 09:31:54 -0500

    Training

---
+++ Hello World2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://www.bing.com/");
+            driver.Manage().Window.Maximize();
+
+            IWebElement searchInput = driver.FindElement(By.Id("sb_form_q"));
+            searchInput.SendKeys("Hello World");
+            //searchInput.SendKeys(Keys.Enter);
+            //.Click();
+
+            IWebElement buttonSearch = driver.FindElement(By.Id("sb_form_go"));
+
+            buttonSearch.Click();
+            driver.Close();
+        }
+
+      
+    }
+}
